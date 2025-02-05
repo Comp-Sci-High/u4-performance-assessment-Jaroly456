dd2f047532fe8fab2f34a491fece7deff7c1c586
Jaroly456 <jaroly.vidaldiaz25@compscihigh.org>
2025-02-05 15:07:41 +0000

    version 2.2

--- index.js
+++ index.js
@@ -8,21 +8,20 @@ let places =
             name: "Japan",
             description: "A country blooming with cultrual history. I've always wanted to come here to see what life is like. Trying out the foods and visiting the older parts of Japan.",
             imageUrl: "https://example.com/japan.jpg",
-            path: "/japan",
             withWho: "Cousins or friends"
           },
           {
             name: "Italy",
             description: "Although I've been to Itatly before, I've never been here with my cousins who live here. Thus, making my main goal to visit family and watch my favorite volleyball player in the Italian leauge.",
             imageUrl: "https://example.com/italy.jpg",
-            path: "/italy",
+            path: "/place/:2",
             withWho: "Mom side of the family"
           },
           {
             name: "Germany",
             description: "A country I always hear that has fun. Germany has many places to see and I can vouch since I've been there before. Although it would be great to go again wthout being under adult supervision.",
             imageUrl: "https://example.com/australia.jpg",
-            path: "/germany",
+            path: "/place/:3",
             withWho: "Friends"
           },
           {
@@ -36,7 +35,7 @@ let places =
             name: "South Korea",
             description: "Home to one of my top 3 food cultures. South Korea has a unique deli experience than we do in New York, which are much better than we have. ",
             imageUrl: "https://example.com/southafrica.jpg",
-            path: "/korea",
+            path: "/place/:5",
             withWho: "Solo or with friends"
           }
         ]
@@ -62,7 +61,7 @@ app.get("/destinations", (req, res) => {
 
 app.get("/:country", (req, res) => {
   const country = req.params.country
-  res.status(200).render("place.ejs", places)
+  res.status(200).render("place.ejs", places.countries[country])
 })
 
 
